96e8bd68baa863297901ca7270a911193a60b18e
Pierre Larmande <pierre.larmande@ird.fr>
2019-10-02 09:54:54 +0700

    Adding replacements on URI

--- riceKB/ensmbl.rdf.py
+++ riceKB/ensmbl.rdf.py
@@ -23,17 +23,65 @@ TODO:
 '''
 __author__  = "larmande"
 
+prefixes = {}
+patern1 = "^<http://rdf.ebi.ac.uk/resource/ensembl/>"
+def ensemblParser(files):
+    """
+    parse the ensembl rdf file
+    :param files: path of the file
+    :return: python objects corresponding to prefix and triples
+    """
+    if(os.path.isfile(str(files))):
+        print "***************** Parsing Esembl RDF data ********************\n"
+        with open(files) as fp:
+            for line in fp:
+                if(re.match(r'^@prefix', line)):
+                    #print line
+                    prefix, value = re.split(':\s+', line)
+                    value = re.sub('\.\n$', '', value)
+                    prefixes[prefix]=value
+                else:
+                    if re.findall("<http://rdf.ebi.ac.uk/resource/ensembl/",line):
+                        line = re.sub('<http://rdf\.ebi\.ac\.uk/resource/ensembl/\d*/?', 'http://www.southgreen.fr/agrold/resource/', line)
+                    if re.findall("term:inEnsemblAssembly",line):
+                        line = re.sub('term:inEnsemblAssembly',
+                                      'term:inAssembly', line)
+                    if re.findall("term:inEnsemblSchemaNumber",line):
+                        line = re.sub('term:inEnsemblSchemaNumber',
+                                      'term:inSchemaNumber', line) # a supprimer ou modifier
+                    if re.findall("<http://rdf.ebi.ac.uk/resource/ensembl.transcript/",line):
+                        line = re.sub('<http://rdf\.ebi\.ac\.uk/resource/ensembl\.transcript/',
+                                      'http://www.southgreen.fr/agrold/resource/transcript/', line)
+                    if re.findall("<http://rdf.ebi.ac.uk/resource/ensembl.protein/",line):
+                        line = re.sub('<http://rdf\.ebi\.ac\.uk/resource/ensembl\.protein/',
+                                      'http://www.southreen.fr/agrold/resource/protein/', line)
+                    if re.findall("<http://rdf.ebi.ac.uk/resource/ensembl.exon/",line):
+                        line = re.sub('<http://rdf\.ebi\.ac\.uk/resource/ensembl\.exon/',
+                                      'http://www.southreen.fr/agrold/resource/exon/', line)
+                    if re.findall("term:protein",line):
+                        line = re.sub('term:protein', 'term:Protein', line)
+                    if re.findall("term:protein_coding",line):
+                        line = re.sub('term:protein_coding', 'term:Gene', line)
+
+                    print line
 
 
 
-ROOT_DIR = '/Users/plarmande/workspace2015/data/'
 
-ensemb_files = [ROOT_DIR + 'Oryza_sativa_japonica.txt']
-ensembl_out = '/Users/plarmande/workspace2015/data/'
+    else:
+        print "***************** File not found ********************\n"
+        print files
+
+
+ROOT_DIR = '/Users/plarmande/Downloads/larmande/'
+
+ensembl_files =  os.path.join(ROOT_DIR+'sample.os.ttl')
+ensembl_out = '/Users/plarmande/Downloads/larmande/'
 
 
 pp = pprint.PrettyPrinter(indent=4)
 
+ensemblParser(ensembl_files)
 print "***************** Esembl RDF data ********************\n"
 
 
