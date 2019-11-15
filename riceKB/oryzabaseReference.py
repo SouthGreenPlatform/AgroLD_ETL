8ebae0dff7ec57624020a99199d79a2d302470f5
Pierre Larmande <pierre.larmande@ird.fr>
2019-11-15 16:20:45 +0700

    Code compatibility > 3.5

--- riceKB/oryzabaseReference.py
+++ riceKB/oryzabaseReference.py
@@ -1,24 +1,13 @@
-import gffParser
-import pprint
-from globalVars import *
-from globalVars import base_vocab_ns
+
+from riceKB.globalVars import *
+from riceKB.globalVars import base_vocab_ns
 import re
-import requests
 import os, sys
-import datetime
-import json
-import copy
-import time
 import pandas as pd
-import numpy as np
-from pandas.io.json import json_normalize
-import rdflib
 from rdflib.graph import Graph
 from rdflib import Namespace, Literal, URIRef, BNode
 from rdflib.namespace import RDF, RDFS, OWL, XSD, SKOS, DCTERMS
 from rdflib import URIRef
-import urllib3
-import requests
 
 
 #pp = pprint.PrettyPrinter(indent=4)
@@ -54,9 +43,19 @@ def printGenes(file):
     pubParser(file)
 
 def pubParserPandas(infile):
-    array = pd.read_csv(infile, sep="\t", delimiter=None, dtype='str')
-    print array
+    array = pd.read_csv(infile, sep="\t", delimiter=None, dtype='str', encoding ='latin1', error_bad_lines=False)
     return array
+
+def buildLabelDic(file):
+    labelDic = {}
+    if file:
+        RDFgraph = Graph()
+        RDFgraph.parse(file, format='turtle')
+        print('RDF file exists ****')
+        for subject, p, object in RDFgraph.triples((None, RDFS.label, None)): # for s,p,o in g.triples( (None,  RDF.type, None) ):
+            #print(object)
+            labelDic[str(object)] = subject
+        return labelDic
 
 def referenceRDF(file, rdf_file, output_dir,type='run'):
     rdf_buffer = ''
@@ -64,7 +63,8 @@ def referenceRDF(file, rdf_file, output_dir,type='run'):
     file_o = "reference_oryzabase"
     output_file = os.path.join(output_dir, turtle_file)
     output_rdflib = os.path.join(output_dir, file_o)
-    print "*************Parsing %s genome data ***********\n" % (file)
+
+    print("*************Parsing %s genome data ***********\n" % (file))
 
     output_opener = open(output_file, "w")
     rdflib_writer = open(output_rdflib, "w")
@@ -99,15 +99,13 @@ def referenceRDF(file, rdf_file, output_dir,type='run'):
     pubmed = Namespace(pubmed_uri)
 
     pub_ds = pubParser(file)
-    # global RDFgraph
-    # if rdf_file:
-    #     RDFgraph = Graph()
-    #     RDFgraph.parse(rdf_file, format='turtle')
-    #     print('RDF file exists ****')
+    labelDic = buildLabelDic(rdf_file)
+    labelList = labelDic.keys()
+
 
     print(str(len(pub_ds.keys())) + " Reference parsed")
     output = open('/Users/plarmande/Downloads/error.txt', "w")
-    print "************* %s RDF conversion begins***********\n" % (file)
+    print("************* %s RDF conversion begins***********\n" % (file))
     count = 0
 
     for pub_id in pub_ds.keys():
@@ -117,7 +115,7 @@ def referenceRDF(file, rdf_file, output_dir,type='run'):
                 graph_out = Graph()
                 pubmedid =  URIRef(pubmed+ pub_ds[pub_id]['PubMedId'])
 
-                print pubmedid
+                # print pubmedid
                 count += 1
                 rdf_buffer =''
                 rdf_buffer += pubmed_ns + pub_ds[pub_id]['PubMedId'] + "\n"
@@ -149,9 +147,15 @@ def referenceRDF(file, rdf_file, output_dir,type='run'):
 
                 for symbol in pub_ds[pub_id]['Symbol']:
                     if not re.search(r'_', symbol):
+                        symbol = re.sub('\s+', '', symbol)
                         rdf_buffer += "\t" + base_vocab_ns + "has_symbol" + "\t\t" + '"%s"' % (re.sub('\s+', '',symbol)) + " ;\n"
                         graph_out.add((pubmedid, vocab_ns.has_symbol,
                                       Literal(re.sub('\s+', '',symbol))))
+                        # print(symbol)
+                        if symbol in labelList:
+                        # labelDic.get(symbol,0)
+                            print(symbol)
+                            graph_out.add((URIRef(labelDic[symbol]), DCTERMS.references, pubmedid))
 
                 for synonym in pub_ds[pub_id]['Synonym']:
                     if not re.search(r'_', synonym):
@@ -166,76 +170,27 @@ def referenceRDF(file, rdf_file, output_dir,type='run'):
     print(count)
 
 
-# def RDF_validation(ttl_buffer, ttl_handle, oryid):
-#     try:
-#         temp_file = os.path.join(ROOT_DIR+'temp_graph.ttl')
-#         try_handle = open(temp_file, "w")
-#         try_handle.write(base + "\t" + "<" + base_uri + "> .\n")
-#         try_handle.write(pr + "\t" + rdf_ns + "<" + rdf + "> .\n")
-#         try_handle.write(pr + "\t" + rdfs_ns + "<" + rdfs + "> .\n")
-#         try_handle.write(pr + "\t" + owl_ns + "<" + owl + "> .\n")
-#         try_handle.write(pr + "\t" + base_vocab_ns + "<" + base_vocab_uri + "> .\n")
-#         try_handle.write(pr + "\t" + obo_ns + "<" + obo_uri + "> .\n")
-#         try_handle.write(pr + "\t" + ensembl_ns + "<" + ensembl_plant + "> .\n")
-#         try_handle.write(pr + "\t" + rapdb_gene_ns + "<" + rapdb_gene_uri + "> .\n")
-#         try_handle.write(pr + "\t" + msu_ns + "<" + msu_uri + "> .\n")
-#         try_handle.write(pr + "\t" + tair_l_ns + "<" + tair_l_uri + "> .\n")
-#         try_handle.write(pr + "\t" + up_ns + "<" + uniprot + "> .\n")
-#         try_handle.write(pr + "\t" + ncbi_tax_ns + "<" + ncbi_tax_uri + "> .\n")
-#         try_handle.write(pr + "\t" + dc_ns + "<" + dc_uri + "> .\n")
-#         try_handle.write(pr + "\t" + faldo_ns + "<" + faldo + "> .\n")
-#         try_handle.write(pr + "\t" + xsd_ns + "<" + xsd + "> .\n")
-#         try_handle.write(pr + "\t" + skos_ns + "<" + skos + "> .\n")
-#         try_handle.write(pr + "\t" + sio_ns + "<" + sio_uri + "> .\n")
-#         try_handle.write(pr + "\t" + uniprot_ns + "<" + uniprot_uri + "> .\n")
-#         try_handle.write(pr + "\t" + bibo_ns + "<" + bibo_uri + "> .\n")
-#         try_handle.write(pr + "\t" + pubmed_ns + "<" + pubmed_uri + "> .\n\n")
-#         try_handle.write(ttl_buffer)
-#         try_handle.close()
-#
-#         g = Graph()
-#         g.parse(temp_file, format="turtle")
-#         # output = open(output_file, "w")
-#         # w = Graph()
-#         # g.serialize(destination="file:/Users/plarmande/Downloads/oryzabase_test.ttl", format='xml')
-#         ttl_handle.write(ttl_buffer)
-#     except:
-#         print("Unexpected error:", sys.exc_info()[0])
-#         temp =  oryid + '.ttl'
-#         temp_file = os.path.join(output_dir + temp)
-#         handle = open(temp_file, "w")
-#         handle.write(base + "\t" + "<" + base_uri + "> .\n")
-#         handle.write(pr + "\t" + rdf_ns + "<" + rdf + "> .\n")
-#         handle.write(pr + "\t" + rdfs_ns + "<" + rdfs + "> .\n")
-#         handle.write(pr + "\t" + owl_ns + "<" + owl + "> .\n")
-#         handle.write(pr + "\t" + base_vocab_ns + "<" + base_vocab_uri + "> .\n")
-#         handle.write(pr + "\t" + obo_ns + "<" + obo_uri + "> .\n")
-#         handle.write(pr + "\t" + ensembl_ns + "<" + ensembl_plant + "> .\n")
-#         handle.write(pr + "\t" + rapdb_gene_ns + "<" + rapdb_gene_uri + "> .\n")
-#         handle.write(pr + "\t" + msu_ns + "<" + msu_uri + "> .\n")
-#         handle.write(pr + "\t" + tair_l_ns + "<" + tair_l_uri + "> .\n")
-#         handle.write(pr + "\t" + up_ns + "<" + uniprot + "> .\n")
-#         handle.write(pr + "\t" + ncbi_tax_ns + "<" + ncbi_tax_uri + "> .\n")
-#         handle.write(pr + "\t" + dc_ns + "<" + dc_uri + "> .\n")
-#         handle.write(pr + "\t" + faldo_ns + "<" + faldo + "> .\n")
-#         handle.write(pr + "\t" + xsd_ns + "<" + xsd + "> .\n")
-#         handle.write(pr + "\t" + skos_ns + "<" + skos + "> .\n")
-#         handle.write(pr + "\t" + sio_ns + "<" + sio_uri + "> .\n")
-#         handle.write(pr + "\t" + uniprot_ns + "<" + uniprot_uri + "> .\n")
-#         handle.write(pr + "\t" + bibo_ns + "<" + bibo_uri + "> .\n")
-#         handle.write(pr + "\t" + pubmed_ns + "<" + pubmed_uri + "> .\n\n")
-#         # tigr_g_ns tigr_g_uri
-#         handle.write(ttl_buffer)
-#         pass
-
 
 
 file = '/Users/plarmande/Downloads/Reference_20190617000959.txt'
 rdf_file = '/Users/plarmande/Downloads/OryzabaseGeneListEn_20190528010057.ttl'
+rdf_file2 = '/Users/plarmande/Downloads/oryzabase_test.ttl'
 output_dir = '/Users/plarmande/Downloads/rdf/'
 
-referenceRDF(file,rdf_file, output_dir)
-#pubParserPandas(file)
+vocab_ns = Namespace(base_vocab_uri)
+g = Graph()
+g.parse(rdf_file2, format="turtle")
+count = 0
+output = open('/Users/plarmande/Downloads/benchmark.tsv', "w")
+for s, p, o in g.triples((None, vocab_ns.has_rap_identifier, None)):
+    count +=1
+    # print((s, p, o))
+    print("%s\t%s\n" % (s, o))
+    output.write("%s\t%s\n" % (s, o))
+
+print(count)
+# referenceRDF(file,rdf_file, output_dir)
+# pubParserPandas(file)
 
 #path = '/media/elhassouni/donnees/Noeud-plante-projet/workspace/AgroLD/AgroLD_ETL/test_files/urgi/pseudomolecul_wheat.gff'
 
