9880d83810551bd4c333464f3f067717ae39cbae
Pierre Larmande <pierre.larmande@ird.fr>
2019-12-03 22:29:00 +0800

    Adding synonym better parsing

--- riceKB/uniprotToRDF.py
+++ riceKB/uniprotToRDF.py
@@ -3,9 +3,12 @@ Created on Mar 23, 2015
 
 @author: venkatesan
 '''
-#import sys
-#sys.path.append("/home/venkatesan/Downloads/biopython-1.65/Bio")
-
+# import sys
+# sys.path.append("/home/venkatesan/Downloads/biopython-1.65/Bio")
+# TODO chercher dans les synonyms les pattern RAPDB et MSU pour creer une nouvelle relation
+# TODO AJouter les nouveaux taxons dans le dictionnaire taxon
+# TODO ajouter Prot:uri encodedBy Gene:uri
+# TODO Ajouter pubmed et DOI
 from Bio import SwissProt
 
 from riceKB.globalVars import base_vocab_ns
@@ -15,141 +18,190 @@ import re
 import os
 from _collections import defaultdict
 
-def upToRDF(up_files, rdf_out_dir): #, output_file
-    
+
+def upToRDF(up_files, rdf_out_dir, additional_file):  # , output_file
+
     rdf_file = "uniprot.plants.ttl"
     output_file = os.path.join(rdf_out_dir, rdf_file)
     output_writer = open(output_file, "w")
     rdf_buffer = ''
     prot_counter = 0
+    lookup_list = set()
     pp = pprint.PrettyPrinter(indent=4)
     up_base_uri = "http://purl.uniprot.org/"
-#    up_base_ns = "uniprot_base:"
-    
+    #    up_base_ns = "uniprot_base:"
+    if (additional_file):
+        if (os.path.isfile(str(additional_file))):
+            with open(additional_file, 'r') as infile:
+                for prot in infile:
+                    prot = re.sub('\s+', '', prot)
+                    lookup_list.add(prot)
+
     print("************* Converting Uniprot data to RDF ***************\n")
-    
+
     output_writer.write(base + "\t" + "<" + base_uri + "> .\n")
     output_writer.write(pr + "\t" + rdf_ns + "<" + rdf + "> .\n")
     output_writer.write(pr + "\t" + rdfs_ns + "<" + rdfs + "> .\n")
-    output_writer.write(pr + "\t" + owl_ns + "<" + owl + "> .\n")    
-#    output_writer.write(pr + "\t" + xsd_ns + "<" + xsd + "> .\n")
+    output_writer.write(pr + "\t" + owl_ns + "<" + owl + "> .\n")
+    output_writer.write(pr + "\t" + skos_ns + "<" + skos + "> .\n")
+    output_writer.write(pr + "\t" + dc_ns + "<" + dc_uri + "> .\n")
+    output_writer.write(pr + "\t" + dcterms_ns + "<" + dcterms_uri + "> .\n")
     output_writer.write(pr + "\t" + base_vocab_ns + "<" + base_vocab_uri + "> .\n")
     output_writer.write(pr + "\t" + obo_ns + "<" + obo_uri + "> .\n")
     output_writer.write(pr + "\t" + sio_ns + "<" + sio_uri + "> .\n")
     output_writer.write(pr + "\t" + ncbi_tax_ns + "<" + ncbi_tax_uri + "> .\n")
-#    output_writer.write(pr + "\t" + up_base_ns + "<" + up_base_uri + "> .\n")
+    #    output_writer.write(pr + "\t" + up_base_ns + "<" + up_base_uri + "> .\n")
     output_writer.write(pr + "\t" + up_ns + "<" + uniprot + "> .\n\n")
 
-    
-    for upfile in up_files:
-        file_handle = open(upfile, "r")
+#    for upfile in up_files:
+#        file_handle = open(upfile, "r")
+    with open(up_files, 'r') as file_handle:
         up_records = SwissProt.parse(file_handle)
-#        xrefs = defaultdict(list)
-#        xref_ids = list()             
+        #        xrefs = defaultdict(list)
+        #        xref_ids = list()
         for record in up_records:
             xrefs = defaultdict(list)
             rdf_buffer = ''
             for taxID in record.taxonomy_id:
                 if taxID in taxon_ids:
-#                    rdf_buffer = ''                   
+
                     # Accession
                     if len(record.accessions) > 1:
                         prim_accession = record.accessions.pop(0)
-                        prot_counter += 1 
-                        rdf_buffer += up_ns + prim_accession + "\n" #output_writer.write(up_ns + prim_accession + "\n")
-                        rdf_buffer += "\t" + rdf_ns + "type" + "\t" + base_vocab_ns + "Protein" + " ;\n" #output_writer.write("\t" + rdf_ns + "type" + "\t" + base_vocab_ns + "Protein" + " ;\n")
-                        #rdf_buffer += "\t" + rdfs_ns + "subClassOf" + "\t" + obo_ns + protein_term + " ;\n" #output_writer.write("\t" + rdfs_ns + "subClassOf" + "\t" + obo_ns + protein_term + " ;\n")
+                        prot_counter += 1
+                        rdf_buffer += up_ns + prim_accession + "\n"  # output_writer.write(up_ns + prim_accession + "\n")
+                        rdf_buffer += "\t" + rdf_ns + "type" + "\t" + base_vocab_ns + "Protein" + " ;\n"  # output_writer.write("\t" + rdf_ns + "type" + "\t" + base_vocab_ns + "Protein" + " ;\n")
+                        rdf_buffer += "\t" + dc_ns + "identifier" + "\t" + '"%s"' % (prim_accession) + " ;\n"
+                        rdf_buffer += "\t" + skos_ns + "altLabel" + "\t" + '"%s"' % (prim_accession) + " ;\n"
                         for altID in record.accessions:
-                            rdf_buffer += "\t" + base_vocab_ns + "has_alternative_id" + "\t" + up_ns + altID + " ;\n" #output_writer.write("\t" + rdfs_ns + "subClassOf" + "\t" + obo_ns + protein_term + " ;\n")
+                            rdf_buffer += "\t" + owl_ns + "sameAs" + "\t" + up_ns + altID + " ;\n"  # output_writer.write("\t" + rdfs_ns + "subClassOf" + "\t" + obo_ns + protein_term + " ;\n")
+                            rdf_buffer += "\t" + skos_ns + "altLabel" + "\t" +  '"%s"' % (altID) + " ;\n"  # output_writer.write("\t" + rdfs_ns + "subClassOf" + "\t" + obo_ns + protein_term + " ;\n")
+
                     else:
                         prim_accession = record.accessions[0]
                         prot_counter += 1
                         rdf_buffer += up_ns + prim_accession + "\n"
                         rdf_buffer += "\t" + rdf_ns + "type" + "\t" + base_vocab_ns + "Protein" + " ;\n"
-                        #rdf_buffer += "\t" + rdfs_ns + "subClassOf" + "\t" + obo_ns + protein_term + " ;\n"
-                         
+                        rdf_buffer += "\t" + dc_ns + "identifier" + "\t" + '"%s"' % (prim_accession) + " ;\n"
+                        rdf_buffer += "\t" + skos_ns + "altLabel" + "\t" + '"%s"' % (prim_accession) + " ;\n"
+
+                        # rdf_buffer += "\t" + rdfs_ns + "subClassOf" + "\t" + obo_ns + protein_term + " ;\n"
+
                     # Label
-                    print(record.entry_name)
                     rdf_buffer += "\t" + rdfs_ns + "label" + "\t" + '"%s"' % (record.entry_name) + " ;\n"
-                    
+                    rdf_buffer += "\t" + skos_ns + "prefLabel" + "\t" + '"%s"' % (record.entry_name) + " ;\n"
+
                     # Description
                     if record.description:
                         descriptions = record.description.split(';')
-                        description = descriptions[0][14:]#.lstrip('RecName: Full=')
-                        rdf_buffer += "\t" + base_vocab_ns + "description" + "\t" + '"%s"' % (description) + " ;\n"
-#                    print description
+                        description = descriptions[0][14:]  # .lstrip('RecName: Full=')
+                        rdf_buffer += "\t" + dcterms_ns + "description" + "\t" + '"%s"' % (description) + " ;\n"
+                    #                    print description
 
                     #  Gene Name
-#                    print record.gene_name              
-                    if record.gene_name:
-                        raw_strings = record.gene_name.split(';')
-#                        print raw_strings
-                        string_name = raw_strings[0]
-                        gene_names = string_name.split('=')#record.gene_name.lstrip('Name=')
-                        search_pattern = re.search("\s{", gene_names[1])
-                        if search_pattern:
-                            names = re.split("\s{", gene_names[1])
-                            symbol = names[0]                    
-                            rdf_buffer += "\t" + base_vocab_ns + "has_symbol" + "\t" + '"%s"' % (symbol) + " ;\n" #gene_names[0].lstrip('Name=')
-                        else:
-                            symbol = gene_names[1]
-                            rdf_buffer += "\t" + base_vocab_ns + "has_symbol" + "\t" + '"%s"' % (symbol) + " ;\n"
-#                    print symbol #gene_names[0].lstrip('Name=')
-#                    for name in gene_names:
-#                        name = name.lstrip(' ')
-#                        print name
+                    #                    print record.gene_name
 
+                    if record.gene_name:
+                        for entry in record.gene_name.split(';'):
+                            new_entry = re.sub('\{.+?\}', '', entry)
+                            if re.findall("Name=",new_entry):
+                                value = new_entry.split('=')[1]
+                                for symbol in value.split(','):
+                                    symbol = re.sub('\s+', '', symbol)
+                                    rdf_buffer += "\t" + skos_ns + "prefSymbol" + "\t" + '"%s"' % (
+                                                    symbol) + " ;\n"
+                            if re.findall("Synonyms=", new_entry):
+                                value = new_entry.split('=')[1]
+                                for symbol in value.split(','):
+                                    symbol = re.sub('\s+', '', symbol)
+                                    rdf_buffer += "\t" + skos_ns + "altSymbol" + "\t" + '"%s"' % (
+                                        symbol) + " ;\n"
+                            if re.findall("OrderedLocusNames=", new_entry):
+                                value = new_entry.split('=')[1]
+                                for symbol in value.split(','):
+                                    symbol = re.sub('\s+', '', symbol)
+                                    rdf_buffer += "\t" + skos_ns + "altSymbol" + "\t" + '"%s"' % (
+                                        symbol) + " ;\n"
+                            if re.findall("ORFNames=", new_entry):
+                                value = new_entry.split('=')[1]
+                                for symbol in value.split(','):
+                                    symbol = re.sub('\s+', '', symbol)
+                                    rdf_buffer += "\t" + skos_ns + "altSymbol" + "\t" + '"%s"' % (
+                                        symbol) + " ;\n"
+                    #         raw_strings = ""
+                    #         string_name = raw_strings[0]
+                    #         gene_names = string_name.split('=')  # record.gene_name.lstrip('Name=')
+                    #         search_pattern = re.search("\s{", gene_names[1])
+                    #         if search_pattern:
+                    #             names = re.split("\s{", gene_names[1])
+                    #             symbol = names[0]
+                    #             rdf_buffer += "\t" + skos_ns + "prefSymbol" + "\t" + '"%s"' % (
+                    #             symbol) + " ;\n"  # gene_names[0].lstrip('Name=')
+                    #         else:
+                    #             symbol = gene_names[1]
+                    #             rdf_buffer += "\t" + skos_ns + "prefSymbol" + "\t" + '"%s"' % (symbol) + " ;\n"
+                    # #                    print symbol #gene_names[0].lstrip('Name=')
+                    #                    for name in gene_names:
+                    #                        name = name.lstrip(' ')
+                    #                        print name
                     # Taxon
-                    rdf_buffer += "\t" + base_vocab_ns + "taxon" +  "\t\t" + obo_ns + "NCBITaxon_" + taxID + " ;\n"
+                    # rdf_buffer += "\t" + base_vocab_ns + "taxon" + "\t\t" + obo_ns + "NCBITaxon_" + taxID + " ;\n"
                     rdf_buffer += "\t" + obo_ns + "RO_0002162" + "\t\t" + ncbi_tax_ns + taxID + " ;\n"
-#                   taxID
-                   
+                    #                   taxID
+
                     # Comments
                     if record.comments:
                         raw_comment = ''.join(record.comments)
                         comment = raw_comment.replace('"', '')
-                        rdf_buffer += "\t" + base_vocab_ns + "comment" + "\t" + '"%s"' % (comment) + " ;\n"
-#                   print (comment)
-                    
+                        rdf_buffer += "\t" + rdfs_ns + "comment" + "\t" + '"%s"' % (comment) + " ;\n"
+                    #                   print (comment)
+
                     # Keywords
-#                    print record.keywords
+                    #                    print record.keywords
                     if record.keywords:
                         for keyword in record.keywords:
-#                            print keyword
-                            rdf_buffer += "\t" + base_vocab_ns + "classified_with" + "\t" + '"%s"' % (keyword) + " ;\n"
+                            rdf_buffer += "\t" + base_vocab_ns + "classifiedWith" + "\t" + '"%s"' % (keyword) + " ;\n"
                     # Cross References
-#                    pp.pprint(record.cross_references[0])
-                    
+                    #                    pp.pprint(record.cross_references[0])
+
                     for dbs in record.cross_references:
                         dbname = dbs[0]
                         ids = dbs[1]
                         xrefs[dbname].append(ids)
-                    
+
                     for key in xrefs:
                         if key != "GO":
                             db_namespace = key.lower()
                             for dbid in xrefs[key]:
-                                #rdf_buffer += "\t" + base_vocab_ns + "has_dbxref" + "\t" + "<" + up_base_uri + db_namespace + "/" + dbid + ">" + " ;\n"
+                                # rdf_buffer += "\t" + base_vocab_ns + "has_dbxref" + "\t" + "<" + up_base_uri + db_namespace + "/" + dbid + ">" + " ;\n"
                                 rdf_buffer += "\t" + rdfs_ns + "seeAlso" + "\t" + "<" + up_base_uri + db_namespace + "/" + dbid + ">" + " ;\n"
-                    
-                    # Corss references using blank node
-#                    for key in xrefs:
-#                        rdf_buffer += "\t" + base_vocab_ns + "has_dbxref" + "\t" + "[" + "\n"
-#                        rdf_buffer += "\t" + "\t" +  base_vocab_ns + "dbname" + "\t" + '"%s"' % (key) + " ;\n" #"[" +
-#                        for dbid in xrefs[key]:
-#                            rdf_buffer += "\t" + "\t" + base_vocab_ns + "id" + "\t" + '"%s"' % (dbid) + " ;\n"
-#                        rdf_buffer = re.sub(' ;$', '', rdf_buffer)    
-#                        rdf_buffer += "\t" + "\t" + "]" + " ;\n" 
+                        if key == "GO":
 
-                    rdf_buffer = re.sub(' ;$', ' .\n"', rdf_buffer)
+                            for dbid in xrefs[key]:
+                                rdf_buffer += "\t" + base_vocab_ns + "classifiedWith" + "\t" + obo_ns+ re.sub(':','_',dbid) + " ;\n"
+
+                    # Corss references using blank node
+                    #                    for key in xrefs:
+                    #                        rdf_buffer += "\t" + base_vocab_ns + "has_dbxref" + "\t" + "[" + "\n"
+                    #                        rdf_buffer += "\t" + "\t" +  base_vocab_ns + "dbname" + "\t" + '"%s"' % (key) + " ;\n" #"[" +
+                    #                        for dbid in xrefs[key]:
+                    #                            rdf_buffer += "\t" + "\t" + base_vocab_ns + "id" + "\t" + '"%s"' % (dbid) + " ;\n"
+                    #                        rdf_buffer = re.sub(' ;$', '', rdf_buffer)
+                    #                        rdf_buffer += "\t" + "\t" + "]" + " ;\n"
+
+                    rdf_buffer = re.sub(' ;$', ' .\n', rdf_buffer)
                     output_writer.write(rdf_buffer)
         file_handle.close()
     output_writer.close()
     print("Number of Proteins: %s\n" % (str(prot_counter)))
     print("*************** UniProt RDF conversion completed ************\n")
-                
+
+
 #                   pp.pprint(record.cross_references) #taxonomy_id cross_references comments description keywords gene_name molecule_type
 
-# up_dir = '/Volumes/LaCie/AGROLD/agroLD_data_update_mai_2017/data/uniprot/*.dat'
-# ROOT_DIR='/Users/plarmande/Downloads'
-# upToRDF(up_dir, ROOT_DIR )
+# up_dir = '/Volumes/LaCie/AGROLD/argoLD_project_all_data_avril_2017/data/uniport/*.dat'
+# up_dir = '/Volumes/LaCie/AGROLD/argoLD_project_all_data_avril_2017/data/uniport/*.dat'
+up_dir = '/Users/plarmande/workspace2015/AgroLD.old/test_files/uniprot/uniprot_sample.dat'
+ROOT_DIR = '/Users/plarmande/Downloads'
+# upToRDF(up_dir, ROOT_DIR,'/Volumes/LaCie/AGROLD/data_update_2019/Rice_Genome_Hub/uniprot_id.txt')
+upToRDF(up_dir, ROOT_DIR, '/Users/plarmande/workspace2015/datasets/uniprot_id.txt')
