6a151311bdd1766396685b0926313b500ef75ff3
Pierre Larmande <pierre.larmande@ird.fr>
2019-12-21 14:16:31 +0700

    added function getRDFHeaders

--- riceKB/utils.py
+++ riceKB/utils.py
@@ -84,3 +84,21 @@ def getFaldoRegion(taxon_id,seqid,start,end,strand):
     genome_buffer += "\t" + faldo_ns + "reference" + "\t" + "<" + chromosome_uri + taxon_id + \
                      "/" + re.sub('Os|Chr', '', seqid) + "> .\n\n"
     return genome_buffer
+
+def getRDFHeaders():
+    headersBuffer = ''
+    headersBuffer += base + "\t" + "<" + base_uri + "> .\n"
+    headersBuffer += pr + "\t" + rdf_ns + "<" + rdf + "> .\n"
+    headersBuffer += pr + "\t" + rdfs_ns + "<" + rdfs + "> .\n"
+    headersBuffer += pr + "\t" + xsd_ns + "<" + xsd + "> .\n"
+    headersBuffer += pr + "\t" + owl_ns + "<" + owl_uri + "> .\n"
+    headersBuffer += pr + "\t" + dc_ns + "<" + dc_uri + "> .\n"
+    headersBuffer += pr + "\t" + skos_ns + "<" + skos + "> .\n"
+    headersBuffer += pr + "\t" + base_vocab_ns + "<" + base_vocab_uri + "> .\n"
+    headersBuffer += pr + "\t" + obo_ns + "<" + obo_uri + "> .\n"
+    headersBuffer += pr + "\t" + chromosome_ns + "<" + chromosome_uri + "> .\n"
+    headersBuffer += pr + "\t" + interpro_ns + "<" + interpro_uri + "> .\n"
+    headersBuffer += pr + "\t" + ncbi_tax_ns + "<" + ncbi_tax_uri + "> .\n"
+    headersBuffer += pr + "\t" + faldo_ns + "<" + faldo + "> .\n"
+    # Ajout du prefix pour la realese des donnees
+    headersBuffer += pr + "\t" + base_resource_ns + "<" + base_resource_uri + "> .\n\n"
