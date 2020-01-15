74aa4f211de04f5fe31a6bf4e8d8eec985f99dfc
Pierre Larmande <pierre.larmande@ird.fr>
2020-01-15 10:00:34 +0700

    bug fixes

--- riceKB/riceNetV2.py
+++ riceKB/riceNetV2.py
@@ -30,6 +30,7 @@ def goldStandard(infile,outfile):
         nb_line = 0
         rdf_writer.write(str(getRDFHeaders()))
         for line in file:
+            nb_line += 1
             buffer = ''
             gene1, gene2 = line.split('\t')
             gene2 = re.sub('\s','',gene2)
@@ -41,8 +42,115 @@ def goldStandard(infile,outfile):
             print(buffer)
         print("nb de lignes traitees:" + str(nb_line))
 
-path = '/Users/plarmande/workspace2015/datasets/RiceNet_GoldSandard.txt'
-path_output = '/Users/plarmande/workspace2015/datasets/RiceNet_GoldSandard.ttl' # The output
+
+def coexpression(infile,outfile):
+    with open(infile, 'r', encoding='utf-8') as file:
+        rdf_writer = open(outfile, "w")
+        nb_line = 0
+        rdf_writer.write(str(getRDFHeaders()))
+        for line in file:
+            random_int = np.random.random_integers(100)
+            nb_line += 1
+            buffer = ''
+            gene1, gene2, score = line.split('\t')
+            score = re.sub('\s','',score)
+            score = float(score)
+            buffer += "<" + base_resource_uri  + gene1 + ">"
+            buffer += "\t" + obo_ns + "INO_0000231" + "\t\t"
+            buffer += "<" + base_resource_uri  + "association/" +gene1 + "_" + gene2 + "_" + str(round(score*1000)*random_int) +  "> .\n\n"
+
+            buffer += "<" + base_resource_uri + "association/" + gene1 + "_" + gene2 + "_" + str(round(score*1000)*random_int) + ">\n"
+            buffer +=  "\t" + base_vocab_ns + "associationValue"+  "\t"  + "\"" + str(score) + "\"^^xsd:float ;\n"
+            buffer +=  "\t" + base_vocab_ns + "associationTarget" + "\t" + "<" + base_resource_uri + gene2 + "> .\n\n"
+
+            buffer += "<" + base_resource_uri + gene2 + ">"
+            buffer += "\t" + obo_ns + "INO_0000231" + "\t\t"
+            buffer += "<" + base_resource_uri + "association/" + gene2 + "_" + gene1 + "_" + str(
+                round(score * 1000) * random_int) + "> .\n\n"
+
+            buffer += "<" + base_resource_uri + "association/" + gene2 + "_" + gene1 + "_" + str(
+                round(score * 1000) * random_int) + ">\n"
+            buffer += "\t" + base_vocab_ns + "associationValue" + "\t" + "\"" + str(score) + "\"^^xsd:float ;\n"
+            buffer += "\t" + base_vocab_ns + "associationTarget" + "\t" + "<" + base_resource_uri + gene1 + "> ;\n"
+            buffer = re.sub(' ;$', ' .\n', buffer)
+            rdf_writer.write(buffer)
+            print(buffer)
+        print("nb de lignes traitees:" + str(nb_line))
+
+def ppi(infile,outfile):
+    with open(infile, 'r', encoding='utf-8') as file:
+        rdf_writer = open(outfile, "w")
+        nb_line = 0
+        rdf_writer.write(str(getRDFHeaders()))
+        for line in file:
+            random_int = np.random.random_integers(100)
+            nb_line += 1
+            buffer = ''
+            gene1, gene2, score = line.split('\t')
+            score = re.sub('\s', '', score)
+            score = float(score)
+            buffer += "<" + base_resource_uri + gene1 + ">"
+            buffer += "\t" + obo_ns + "RO_0002328" + "\t\t"
+            buffer += "<" + base_resource_uri + "association/" + gene1 + "_" + gene2 + "_" + str(
+                round(score * 1000) * random_int) + "> .\n\n"
+
+            buffer += "<" + base_resource_uri + "association/" + gene1 + "_" + gene2 + "_" + str(
+                round(score * 1000) * random_int) + ">\n"
+            buffer += "\t" + base_vocab_ns + "associationValue" + "\t" + "\"" + str(score) + "\"^^xsd:float ;\n"
+            buffer += "\t" + base_vocab_ns + "associationTarget" + "\t" + "<" + base_resource_uri + gene2 + "> .\n\n"
+
+            buffer += "<" + base_resource_uri + gene2 + ">"
+            buffer += "\t" + obo_ns + "RO_0002328" + "\t\t"
+            buffer += "<" + base_resource_uri + "association/" + gene2 + "_" + gene1 + "_" + str(
+                round(score * 1000) * random_int) + "> .\n\n"
+
+            buffer += "<" + base_resource_uri + "association/" + gene2 + "_" + gene1 + "_" + str(
+                round(score * 1000) * random_int) + ">\n"
+            buffer += "\t" + base_vocab_ns + "associationValue" + "\t" + "\"" + str(score) + "\"^^xsd:float ;\n"
+            buffer += "\t" + base_vocab_ns + "associationTarget" + "\t" + "<" + base_resource_uri + gene1 + "> ;\n"
+            buffer = re.sub(' ;$', ' .\n', buffer)
+            rdf_writer.write(buffer)
+            print(buffer)
+        print("nb de lignes traitees:" + str(nb_line))
+
+def phylogenetic(infile,outfile):
+    with open(infile, 'r', encoding='utf-8') as file:
+        rdf_writer = open(outfile, "w")
+        nb_line = 0
+        rdf_writer.write(str(getRDFHeaders()))
+        for line in file:
+            random_int = np.random.random_integers(100)
+            nb_line += 1
+            buffer = ''
+            gene1, gene2, score = line.split('\t')
+            score = re.sub('\s', '', score)
+            score = float(score)
+            buffer += "<" + base_resource_uri + gene1 + ">"
+            buffer += "\t" + obo_ns + "SO_0000857" + "\t\t"
+            buffer += "<" + base_resource_uri + "association/" + gene1 + "_" + gene2 + "_" + str(
+                round(score * 1000) * random_int) + "> .\n\n"
+
+            buffer += "<" + base_resource_uri + "association/" + gene1 + "_" + gene2 + "_" + str(
+                round(score * 1000) * random_int) + ">\n"
+            buffer += "\t" + base_vocab_ns + "associationValue" + "\t" + "\"" + str(score) + "\"^^xsd:float ;\n"
+            buffer += "\t" + base_vocab_ns + "associationTarget" + "\t" + "<" + base_resource_uri + gene2 + "> .\n\n"
+
+            buffer += "<" + base_resource_uri + gene2 + ">"
+            buffer += "\t" + obo_ns + "SO_0000857" + "\t\t"
+            buffer += "<" + base_resource_uri + "association/" + gene2 + "_" + gene1 + "_" + str(
+                round(score * 1000) * random_int) + "> .\n\n"
+
+            buffer += "<" + base_resource_uri + "association/" + gene2 + "_" + gene1 + "_" + str(
+                round(score * 1000) * random_int) + ">\n"
+            buffer += "\t" + base_vocab_ns + "associationValue" + "\t" + "\"" + str(score) + "\"^^xsd:float ;\n"
+            buffer += "\t" + base_vocab_ns + "associationTarget" + "\t" + "<" + base_resource_uri + gene1 + "> ;\n"
+            buffer = re.sub(' ;$', ' .\n', buffer)
+            rdf_writer.write(buffer)
+            print(buffer)
+        print("nb de lignes traitees:" + str(nb_line))
+
+path = '/Users/plarmande/workspace2015/datasets/OS-PG.txt'
+path_output = '/Users/plarmande/workspace2015/datasets/RiceNet_OS-PG.ttl' # The output
 
 #pp.pprint(ds)    # For to see in teminal the parsing
 
@@ -50,4 +158,6 @@ path_output = '/Users/plarmande/workspace2015/datasets/RiceNet_GoldSandard.ttl' 
 
 #RDFConverter(ds, path_output)
 
-goldStandard(path,path_output)
+#goldStandard(path,path_output)
+#coexpression(path,path_output)
+ppi(path,path_output)
