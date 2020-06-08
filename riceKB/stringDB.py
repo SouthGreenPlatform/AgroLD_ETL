fbd07ab8774306f728da0b8d8c5e6e0f63e6fa85
Pierre Larmande <pierre.larmande@ird.fr>
2020-06-08 16:52:57 +0700

    added example data

--- riceKB/stringDB.py
+++ riceKB/stringDB.py
@@ -10,13 +10,15 @@ This module contains Parsers, RDF converters and generic functions for handling 
     2) Fix Gramene record trailing space in the parser, now it is being handled in the RDF converter
     3) better Error handling
 @author: venkatesan
-'''
 
 '''
-protein1 protein2 combined_score
-4530.OS01T0100100-01 4530.OS05T0466900-01 208
-'''
-
+# header of taxonid.protein.links.v.txt
+# protein1 protein2 combined_score
+# 4530.OS01T0100100-01 4530.OS05T0466900-01 208
+# header of taxonid.protein.actions.v.txt
+# item_id_a	item_id_b	mode	action	is_directional	a_is_acting	score
+# 4530.OS01T0100100-01	4530.OS01T0179700-01	activation	activation	t	t	227
+# 4530.OS01T0100100-01	4530.OS01T0179700-01	binding		f	f	194
 import pprint
 from riceKB.globalVars import *
 from riceKB.utils import *
