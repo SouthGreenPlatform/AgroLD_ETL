d49c99f0bc0f5d35a320d9b3539c3f3a705e2530
larmande <pierre.larmande@ird.fr>
2022-08-25 16:56:57 +0200

    adding RDF graph validation

--- riceKB/GFF2RDF.py
+++ riceKB/GFF2RDF.py
@@ -1,64 +1,34 @@
-from __future__ import with_statement
+import sys
 from riceKB.globalVars import *
-from collections import namedtuple
-import gzip
-import urllib3
-import re
+from riceKB.globalVars import base_vocab_ns
+from riceKB.gffParser import *
+from riceKB.utils import *
 import pprint
+import re
+import os
 
+
+'''
+Created on Dec, 2019
+The GFF2RDF module is created as part of the AgroLD project.
+
+This module contains Parsers, RDF converters and generic functions for handling genomeHub data
+It runs with several files downloaded from MSU annotation project http://rice.plantbiology.msu.edu/
+
+1 - First define your input and output path to filename 
+path = ''
+path_output = ''
+2 - Pass the arguments to genParser function
+ds = parseGFF3(path)
+
+3 - run the program 
+RDFConverter(ds, path_output)
+
+@author: larmande
+'''
 
 __author__ = 'larmande'
 
-
-#Initialized GeneInfo named tuple. Note: namedtuple is immutable
-gffInfoFields = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
-GFFRecord = namedtuple("GFFRecord", gffInfoFields)
-attrib = {}
-def parseGFFAttributes(attributeString):
-    """Parse the GFF3 attribute column and return a dict"""
-    if attributeString == ".": return {}
-    ret = {}
-    for attribute in attributeString.split(";"):
-        key, value = attribute.split("=")
-        key = re.sub('"','', key)
-        value = re.sub('"','', value)
-        ret[key] = value
-        attrib[key]=value
-    return ret
-
-def parseGFF3(filename):
-    """
-    A minimalistic GFF3 format parser.
-    Yields objects that contain info about a single GFF3 feature.
-
-    Supports transparent gzip decompression.
-    """
-    #Parse with transparent decompression
-    map_ds = list()
-    openFunc = gzip.open if filename.endswith(".gz") else open
-    with openFunc(filename) as infile:
-        for line in infile:
-            if line.startswith("#"): continue
-            parts = line.strip().split("\t")
-            #If this fails, the file format is not standard-compatible
-            assert len(parts) == len(gffInfoFields)
-            #Normalize data
-            normalizedInfo = {
-                "seqid": None if parts[0] == "." else re.sub('"','', parts[0]),
-                "source": None if parts[1] == "." else re.sub('"','', parts[1]),
-                "type": None if parts[2] == "." else re.sub('"','', parts[2]),
-                "start": None if parts[3] == "." else int(parts[3]),
-                "end": None if parts[4] == "." else int(parts[4]),
-                "score": None if parts[5] == "." else float(parts[5]),
-                "strand": None if parts[6] == "." else re.sub('"','', parts[6]),
-                "phase": None if parts[7] == "." else re.sub('"','', parts[7]),
-                "attributes": parseGFFAttributes(parts[8])
-            }
-            map_ds.append(normalizedInfo)
-            #Alternatively, you can emit the dictionary here, if you need mutability:
-            #    yield normalizedInfo
-            #yield GFFRecord(**normalizedInfo)
-        return map_ds
 
 
 def RDFConverter(ds, output_file):
@@ -221,10 +191,14 @@ def RDFConverter(ds, output_file):
             rdf_writer.write(genome_buffer)
         # break
 
+
 print(attrib)
 pp = pprint.PrettyPrinter(indent=4)
 path = '/Users/pierre/workspace2015/datasets/Oryza_sativa_Kitaake_3.1.gff3'
 path_output = '/Users/pierre/workspace2015/datasets/Oryza_sativa_Kitaake_3.1.ttl'
+#path = '/opt/TOS_DI-20141207_1530-V5.6.1/workspace/gff_data_orygeneDB/File_test/indicaCantTest.gff3'
+#path = '/media/elhassouni/donnees/Noeud-plante-projet/workspace/AgroLD/AgroLD_ETL/test_files/urgi/pseudomolecul_wheat.gff'
+
 ds= parseGFF3(path)
 
 RDFConverter(ds, path_output)
